b2022ef17e3aa3730863e0056ac87a43796838ed
Juan Fernando Donadio <jf.donadio@gmail.com>
2021-02-10 19:00:09 -0300

    sepparated CI tasks according branch, also aesthetical changes

--- app/hello-world.py
+++ app/hello-world.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return f"Hello, World from {getfqdn()}!! =)"
+    return f"<br><p><h1>&nbsp;&nbsp;&nbsp;&nbsp;Hello World, from {getfqdn()}  =)</h1>"
 
 
 if __name__ == "__main__":
